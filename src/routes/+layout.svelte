571096320aa665ca99bf69227416cab6daca8274
Trong Luong Duc <luongductrong2004@gmail.com>
2026-09-05 15:38:08 +0700

    chore: Add site metadata to layout
    This change adds a page title and meta description to the root layout so the app has proper browser tab metadata and basic SEO information.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,8 @@
 </script>
 
 <svelte:head>
+	<title>gh-vault</title>
+	<meta name="description" content="A simple file storage solution" />
 	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
